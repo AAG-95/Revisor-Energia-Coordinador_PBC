03f84efc471ae86a2311ec1d38272b0c64624917
AlonsoFU <alonso.flores1001@gmail.com>
2023-12-11 19:31:41 -0300

    Q

--- revisor_barras.py
+++ revisor_barras.py
@@ -78,13 +78,13 @@ barras = barras.drop(columns=["Zonal_Nivel"])
 
 
 
-lista_sistema = ["SISTEMA A", "SISTEMA B", "SISTEMA C", "SISTEMA D", "SISTEMA E", "SISTEMA F", np.nan, "NaN", "nan"]
+lista_sistema = ["SISTEMA A", "SISTEMA B", "SISTEMA C", "SISTEMA D", "SISTEMA E", "SISTEMA F", np.nan, "NaN", "nan", "na"]
 
-lista_nivel_tension = ["220", "154", "110", "66", "33", "44", "23", "Tx < 25", np.nan, "NaN", "nan"]
+lista_nivel_tension = ["220", "154", "110", "66", "33", "44", "23", "Tx < 25", np.nan, "NaN", "nan", "na"]
 
-barras = barras[~(barras["Zonal"].isin(lista_sistema) | barras["Zonal"].isna())]
+barras = barras[(barras["Zonal"].isin(lista_sistema) | barras["Zonal"].isna())]
 
-barras = barras[~(barras["Nivel Tensión Zonal"].isin(lista_nivel_tension) | barras["Nivel Tensión Zonal"].isna())]
+barras = barras[(barras["Nivel Tensión Zonal"].isin(lista_nivel_tension) | barras["Nivel Tensión Zonal"].isna())]
 
 barras.to_csv(carpeta + "Sistema_por_Barra.csv", sep=";", encoding="latin1", index=False)
 
