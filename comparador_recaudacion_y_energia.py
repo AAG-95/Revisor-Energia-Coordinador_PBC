7351cccf166a4cfb5150f69c7f182b866f9af7c9
AlonsoFU <alonso.flores1001@gmail.com>
2023-12-18 17:49:01 -0300

    ds

--- comparador_recaudacion_y_energia.py
+++ comparador_recaudacion_y_energia.py
@@ -39,9 +39,15 @@ for par in pares_lista:
         + "-_-"
         + df_energia["Clave"].astype(str)
         + "-_-"
-        + df_energia["Suministrador_final"].astype(str)
-        
+        + df_energia["Suministrador_final"].astype(str)   
     )
+
+    # change column name Medidas_2 to Energía Balance [kWh] and make all values negative
+    df_energia["Medida 2"] = df_energia["Medida 2"] * -1
+    df_energia.rename(columns={"Medida 2": "Energía Balance [kWh]"}, inplace=True)    
+    
+    # Mantain only column Barra-Clave-Suministrador-Mes and Energía Balance [kWh]
+    df_energia = df_energia[["Barra-Clave-Suministrador-Mes", "Energía Balance [kWh]"]]
 
     df_recaudacion = pd.read_csv(
         carpeta_recaudación
@@ -60,16 +66,17 @@ for par in pares_lista:
         df_recaudacion["Empresa_Planilla_Recauda_Cliente"] == 1
     ]
 
-
     df_recaudacion["Barra-Clave-Suministrador-Mes"] = (
         df_recaudacion["Barra"].astype(str)
         + "-_-"
         + df_recaudacion["Clave"].astype(str)
         + "-_-"
-        + df_recaudacion["Recaudador"].astype(str)
-        
+        + df_recaudacion["Recaudador"].astype(str)    
     )
     
+    # Mantain only column Barra-Clave-Suministrador-Mes and Energía [kWh]
+    df_recaudacion = df_recaudacion[["Barra-Clave-Suministrador-Mes", "Energía [kWh]"]]
+
     # Add column Energía [kWh] from df_recaudación into 
     # df_energía by column Barra-Clave-Suministrador-Mes. Conservar Columna Barra-Clave-Suministrador-Mes
     df_combinado = pd.merge(df_energia, df_recaudacion[["Barra-Clave-Suministrador-Mes", "Energía [kWh]"]], on="Barra-Clave-Suministrador-Mes", how="left")
@@ -77,6 +84,31 @@ for par in pares_lista:
     print(df_energia["Barra-Clave-Suministrador-Mes"].unique())
     print(df_recaudacion["Barra-Clave-Suministrador-Mes"].unique())
     print(df_combinado["Barra-Clave-Suministrador-Mes"])
+    
 
+
+    # change column name Energía [kWh] to Energía Recaudada [kWh] and make all values negative
+    df_combinado.rename(columns={"Energía [kWh]": "Energía Declarada [kWh]"}, inplace=True)
+    
+    df_combinado["Energía Balance [kWh]"] = df_combinado["Energía Balance [kWh]"].astype(str).str.replace(',', '.').astype(float)
+
+    df_combinado["Energía Declarada [kWh]"] = df_combinado["Energía Declarada [kWh]"].astype(str).str.replace(',', '.').astype(float)
+    
+    # New Column difference between Energía Balance [kWh] and Energía Recaudada [kWh]
+    df_combinado["Diferencia Energía [kWh]"] = df_combinado["Energía Balance [kWh]"] - df_combinado["Energía Declarada [kWh]"]
+    
+    # New Column percentage between Energía Balance [kWh] and Energía Recaudada [kWh]
+    df_combinado["% Diferencia Energía"] = (df_combinado["Energía Balance [kWh]"] - df_combinado["Energía Declarada [kWh]"])/ df_combinado["Energía Balance [kWh]"]
+
+    # New Column if df_combinado["Energía Balance [kWh]"] is 0 or nan then "Clave Obsoleta", else if df_combinado["Energía Balance [kWh]"] is not 0 or nan and df_combinado["Diferencia Energía [kWh]"] is 0 or nan then "Clave no informada en RCUT", if df_combinado["% Diferencia Energía"] is greater than 0.05 then "Diferencia Energía con Diferencias", else "Diferencia Energía sin Diferencias"
+
+    df_combinado["Tipo"] = df_combinado.apply(lambda x: "Clave Obsoleta" if pd.isna(x["Energía Balance [kWh]"]) or x["Energía Balance [kWh]"] == 0 else ("Clave no informada en RCUT" if pd.isna(x["Diferencia Energía [kWh]"]) else ("Diferencia Energía con Diferencias" if x["% Diferencia Energía"] > 0.05 else "Diferencia Energía sin Diferencias")), axis=1)
+
+    # Split
+    df_combinado["Barra"] = df_combinado["Barra-Clave-Suministrador-Mes"].str.split("-_-", expand=True)[0]
+    df_combinado["Clave"] = df_combinado["Barra-Clave-Suministrador-Mes"].str.split("-_-", expand=True)[1] 
+    df_combinado["Suministrador"] = df_combinado["Barra-Clave-Suministrador-Mes"].str.split("-_-", expand=True)[2]
+
+    
 
     a = 5
