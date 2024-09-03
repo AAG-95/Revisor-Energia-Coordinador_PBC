528b74a0962dc4dfb7e1e6040569025e04bf6b2b
alonso.flores <alonso.flores@coordinador.cl>
2024-09-03 15:33:09 -0400

    hola

--- creador_listado_clientes_energia.py
+++ creador_listado_clientes_energia.py
@@ -17,11 +17,52 @@ import warnings
 warnings.filterwarnings("ignore", category=pd.errors.DtypeWarning)
 
 class CreadorListaClientesBalance:
+    """
+    Esta clase se encarga de procesar y organizar los datos de clientes individuales
+    a partir de archivos ZIP. El objetivo principal es obtener los datos de los clientes
+    que aparecen en los balances de energía mensuales, para luego guardar los resultados
+    en un archivo Excel.
+
+    Atributos:
+    lista_meses (list): Lista de meses a procesar.
+    ruta_homologa_propietarios (str): Ruta del archivo de homologación de propietarios.
+    ruta_control_versiones (str): Ruta del archivo de control de versiones.
+    ruta_retiros_historicos_L (str): Ruta del archivo de retiros históricos de clientes libres.
+    ruta_retiros_historicos_R (str): Ruta del archivo de retiros históricos de clientes regulados.
+    ruta_registro_cambios_empresas (str): Ruta del archivo de registro de cambios de empresas.
+    ruta_registro_cambios_clientes_L (str): Ruta del archivo de registro de cambios de clientes libres.
+    ruta_registro_cambios_clientes_R (str): Ruta del archivo de registro de cambios de clientes regulados.
+    lista_balance_fisico (list): Lista de nombres de hojas de balance físico.
+    rutas_posibles (list): Lista de rutas posibles en los archivos ZIP.
+    
+    Métodos:
+    get_dataframes(): Carga los archivos de homologación de propietarios y control de versiones.
+    procesar_meses(mes): Procesa los datos de los clientes para un mes específico.
+    run(): Ejecuta el programa.
+
+
+    """
     def __init__(self,lista_meses):
+
+        """
+        Inicializa los atributos de la clase. 
+
+        Atributos:
+        lista_meses (list): Lista de meses a procesar.
+        ruta_homologa_propietarios (str): Ruta del archivo de homologación de propietarios.
+        ruta_control_versiones (str): Ruta del archivo de control de versiones.
+        ruta_retiros_historicos_L (str): Ruta del archivo de retiros históricos de clientes libres.
+        ruta_retiros_historicos_R (str): Ruta del archivo de retiros históricos de clientes regulados.
+        ruta_registro_cambios_empresas (str): Ruta del archivo de registro de cambios de empresas.
+        ruta_registro_cambios_clientes_L (str): Ruta del archivo de registro de cambios de clientes libres.
+        ruta_registro_cambios_clientes_R (str): Ruta del archivo de registro de cambios de clientes regulados.
+        lista_balance_fisico (list): Lista de nombres de hojas de balance físico.
+        rutas_posibles (list): Lista de rutas posibles en los archivos ZIP.
+
+        Devuelve:
+        None
+        """
         #! Month Selection
-        """ self.ventana = gui.VentanaIngresoDatos()
-        self.ventana.iniciar()
-        self.mes = self.ventana.visualizador() """
         self.lista_meses = lista_meses    
         # ? Paths inputs
         self.ruta_homologa_propietarios = r"\\nas-cen1\D.Peajes\\Cargo por Transmisión\02 Repartición\Balances\Homologaciones\Homologacion_Propietarios_Balance_Fisico.xlsx"
@@ -31,6 +72,8 @@ class CreadorListaClientesBalance:
         self.ruta_registro_cambios_empresas = r"\\nas-cen1\D.Peajes\\Cargo por Transmisión\02 Repartición\Balances\Listados de Clientes\Registro de Cambios\Registro_de_Cambios_Empresas.csv"
         self.ruta_registro_cambios_clientes_L = r"\\nas-cen1\D.Peajes\\Cargo por Transmisión\02 Repartición\Balances\Listados de Clientes\Registro de Cambios\Registro_de_Cambios_Clientes_Libres.csv"
         self.ruta_registro_cambios_clientes_R = r"\\nas-cen1\D.Peajes\\Cargo por Transmisión\02 Repartición\Balances\Listados de Clientes\Registro de Cambios\Registro_de_Cambios_Clientes_Regulados.csv"
+
+        # Listado de posibles nombres de hojas de balance físico y rutas posibles en los archivos ZIP
         self.lista_balance_fisico = [
             "REVISION_NORTE_",
             "REVISION_NORTE_DX_",
@@ -53,29 +96,28 @@ class CreadorListaClientesBalance:
             "01 Resultados/01 Resultados/01 Balance de Energía/02 Balance Físico/",
         ]
 
-
-
-
 #! Main Program
     def get_dataframes(self):
-        # ? Get Propietarios Names as they are in the Balance Físico
-        # Get dataframe from self.ruta_homologa_propietarios sheet 'Homologa'
+        """
+        Carga los archivos de homologación de propietarios y control de versiones.
+        """
+
+        # ? Obtener Nombre de Propietarios
+        # Obtener dataframe de self.ruta_homologa_propietarios en la hoja 'Homologa'
         self.df_homologa_propietarios = pd.read_excel(
             self.ruta_homologa_propietarios, sheet_name="Homologa"
         )
 
-        # ? Get Versión
-        # Get dataframe from self.ruta_control_versiones sheet 'Versiones'
+        # ? Obtener Versiones de Balance
+        # Obtener dataframe de self.ruta_control_versiones en la hoja 'Versiones'
         self.df_control_versiones = pd.read_excel(
             self.ruta_control_versiones, sheet_name="Versiones", header=None
         )
 
+        # Obtener columnas 5, 8 y 9 de self.df_control_versiones
         self.df_control_versiones = fc.ObtencionDatos().obtencion_tablas_clientes(
             self.df_control_versiones, 5, 8, 9
         )
-
-# ? Listado de Posibles Archivos y Rutas
-# List of sheets to read from ZIP file
 
     def procesar_meses(self,mes):
         
@@ -83,49 +125,50 @@ class CreadorListaClientesBalance:
             mes_fecha = fc.ConversionDatos().convertir_fecha(mes)
             mes_numeral = fc.ConversionDatos().convertir_fecha_numeral(mes)
 
-            # Get previous month from mes_fecha
+            # Obteniendo mes anterior
             mes_anterior = pd.to_datetime(mes_fecha - relativedelta(months=1)).strftime('%d-%m-%Y')
             mes_fecha = mes_fecha.strftime('%d-%m-%Y')
 
-            # Get Value from column Versiones when mes fecha match value in column Mes
+            # Obtener versión de control de versiones
             version = self.df_control_versiones.loc[
             pd.to_datetime(self.df_control_versiones["Mes"]).dt.strftime('%d-%m-%Y') == mes_fecha, "Versión"
             ].iloc[0]
 
-            # Path to ZIP file, Balance Físico
+            # Ruta al archivo ZIP
             ruta_zip = rf"\\nas-cen1\D.Peajes\Cargo por Transmisión\02 Repartición\Balances\Balances de Energía\Archivos Fuente\20{mes_numeral[:2]}\{mes}-{version}.zip"
 
             # ? Dataframes a guardar
-            listado_clientes = []
-            listado_clientes_R = []
-            listado_clientes_L = []
-            ruta_correcta = None
+            listado_clientes = [] # Listado de Clientes L y R
+            listado_clientes_R = [] # Listado de Clientes R
+            listado_clientes_L = [] # Listado de Clientes L
+            ruta_correcta = None # Ruta correcta en ZIP
 
             print("Mes a evaluar: " + str(mes) + " // Versión a evaluar: " + str(version))
 
+            #  Abrir archivo ZIP
             with zipfile.ZipFile(ruta_zip) as myzip:
                 for i in self.lista_balance_fisico:
                     print("Archivo " + i + " Analizado en " + str(mes) + ":", end="")
-
+                    # Para cada ruta posible en ZIP
                     for path in self.rutas_posibles:
                         try:
                             with myzip.open(path + i + mes_numeral + ".xls") as myfile:
                                 df_balance_fisico = pd.read_excel(myfile)
                                 print(f" Ruta en ZIP EXISTE con {path}")
-                                ruta_correcta = path  # store the correct path
-                                break  # if file is found and opened successfully, break the loop
+                                ruta_correcta = path # Guardar la ruta correcta
+                                break  # Si se encuentra el archivo, salir del loop
                         except KeyError:
-                            continue  # if file is not found, try the next path
+                            continue  # Si no se encuentra el archivo, probar con la siguiente ruta
                     else:
                         print(f" Ruta NO EXISTE para {i}")
-                        continue  # if file is not found for any path, move on to the next i
+                        continue  # Si no se encuentra el archivo, probar con el siguiente archivo
 
                     with myzip.open(ruta_correcta + i + mes_numeral + ".xls") as myfile:
                         df_balance_fisico = pd.read_excel(
                             myfile, sheet_name="Balance por Barra", header=None
                         )
 
-                        # Replace (0/1) in other columns
+                        # Obtener columnas 6, 2 y 17 de df_balance_fisico
                         df_balance_fisico.iloc[:, 11] = df_balance_fisico.iloc[:, 11].replace(
                             "(0/1)", "(0/1).1"
                         )
@@ -133,37 +176,38 @@ class CreadorListaClientesBalance:
                             "(0/1)", "(0/1).2"
                         )
 
-                        # Seleted table from sheet Balance por Barra
+                        # Obtener fila 6 y columnas 2 a 17 de df_balance_fisico
                         df_clientes = fc.ObtencionDatos().obtencion_tablas_clientes(
                             df_balance_fisico, 6, 2, 17
                         )
 
-                        # Drop Column names N if it exists
+                        # Eliminar
                         df_clientes = df_clientes.drop(columns="N")
 
-                        # Change Nombre_2 to Barra
+                        # Cambiar nombre de columnas
                         df_clientes.rename(columns={"Nombre_2": "Barra"}, inplace=True)
-                        # Change (0/1)_2 into Medidas 2 and (0/1)_3 into Medidas 3
+
+                        # Cambiar nombre de columnas
                         df_clientes.rename(
                             columns={"(0/1)_2": "Medida 2", "(0/1)_3": "Medida 3"}, inplace=True
                         )
 
-                        # If values is found in column Barra, replace all nan values in bottom rows from same column with that value and stop when find another value
+                        # Si la columna Barra tiene NaN, rellenar con el valor de la fila anterior
                         df_clientes["Barra"] = df_clientes["Barra"].ffill()
 
-                        # Add month column
+                        # Agregar Columna Mes
                         df_clientes["Mes"] = mes_fecha
 
-                        # Drop Rows from column df_clientes['Nombre'] that contains 'TOTAL' or NaN
+                        # Botar filas con NaN en columnas Nombre y Tipo
                         df_clientes = df_clientes[df_clientes["Nombre"] != "TOTAL"]
                         df_clientes = df_clientes[df_clientes["Nombre"] != "NaN"]
 
-                        # From Column Tipo filter 'R', 'L' or 'L_D'
+                        # Si la columna Tipo tiene R, L o L_D, guardar en listado_clientes
                         retiros_clientes = df_clientes[
                             df_clientes["Tipo"].isin(["R", "L", "L_D"])
                         ]
 
-                        # Merge dataframe df_clientes with self.df_homologa_propietarios based in column Propietario from both df
+                        # Junta la columna Propietario con la homologación de propietarios
                         retiros_clientes = pd.merge(
                             retiros_clientes,
                             self.df_homologa_propietarios,
@@ -171,7 +215,7 @@ class CreadorListaClientesBalance:
                             how="left",
                         )
 
-                        # Check if Column Suministrador_final has NaN, if Column has NaN, print this row and end program
+                        # Verificar si hay NaN en la columna Suministrador_final
                         if retiros_clientes["Suministrador_final"].isnull().values.any():
                             print(
                                 "Columnas con NaN en Suministrador_Final al no coincidir con Propietario:"
@@ -183,7 +227,7 @@ class CreadorListaClientesBalance:
                             )
                             sys.exit()
 
-                        # retiros divided in R and L
+                        # Reemplazar NaN en columnas Empresa y Suministrador_final por "Sin Información"
                         retiros_clientes_R = retiros_clientes[
                             retiros_clientes["Tipo"].isin(["R"])
                         ]
@@ -192,70 +236,70 @@ class CreadorListaClientesBalance:
                             retiros_clientes["Tipo"].isin(["L", "L_D"])
                         ]
 
+                        # Guardar en listado_clientes_R y listado_clientes_L
                         listado_clientes.append(retiros_clientes)
                         listado_clientes_R.append(retiros_clientes_R)
                         listado_clientes_L.append(retiros_clientes_L)
 
-            # ? Concatenate Columns
-            # Concatenate all dataframes from listado_clientes
+            # ? Concatenar Columnas
+            # Concatenar listado_clientes, listado_clientes_R y listado_clientes_L
             df_clientes = pd.concat(listado_clientes)
             df_clientes_R = pd.concat(listado_clientes_R)
             df_clientes_L = pd.concat(listado_clientes_L)
 
-            # ? Reorder Columns
-            # Get a list of column names
+            # ? Reordenar Columnas
+            # Obtener columnas de df_clientes
             cols = df_clientes_L.columns.tolist()
 
-            # Swap the columns 'Propietario' and 'Suministrador_final' by index
+            # Alternar columnas Propietario y Suministrador_final
             indice_propietario = cols.index("Propietario")
             indice_suministrador = cols.index("Suministrador_final")
 
-            
-            # Swap the positions of the two columns in the list
+            # Alternar columnas Propietario y Suministrador_final
             cols[indice_propietario], cols[indice_suministrador] = cols[indice_suministrador], cols[indice_propietario]
 
-            # Reassign the columns of the DataFrame in the new order
+            # Reasignar columnas a df_clientes_L
             df_clientes_L = df_clientes_L[cols]
 
-            #! Output of program-----------------------------------------------------------------
+            #! Salida-----------------------------------------------------------------
 
-            # ? Update Registro Cambios Empresas-------------------------------------------------
-            # Get unique values from df_clientes
+            # ? Actualizar Registro Cambios Empresas-------------------------------------------------
+            # Obtener Valores Unicos df_clientes
             df_clientes_unique = df_clientes.drop_duplicates(subset=["Suministrador_final"])
-            # Get only column Suministrador_final and mes_fecha
+            # Obtener solo columna Suministrador_final y mes_fecha
             df_clientes_unique = df_clientes_unique[["Suministrador_final", "Mes"]]
 
-            # order by column Suministrador_final alfabetically
+            # Ordenar por Suministrador_final
             df_clientes_unique = df_clientes_unique.sort_values(by=["Suministrador_final"])
 
-            # Get database from self.ruta_registro_cambios_empresas
+            # Obtener Registro Cambios Empresas
             df_registro_cambios_empresas = pd.read_csv(self.ruta_registro_cambios_empresas, sep=";")
 
-            # Change column mes_fecha to datetime, example input 1-08-2020
+            # Cambiar formato de Mes a dd-mm-YYYY
             df_registro_cambios_empresas["Mes"] = pd.to_datetime(
                 df_registro_cambios_empresas["Mes"]
             ).dt.strftime('%m-%d-%Y')
 
-            # Filter month column from df_registro_cambios_empresas with mes_anterior
+            # Filtrar mes_anterior
             df_registro_cambios_empresas_mes_anterior = df_registro_cambios_empresas[
                 df_registro_cambios_empresas["Mes"] == mes_anterior
             ]
 
-            # Get Values that are in df_clientes_unique and not in df_registro_cambios_empresas_mes_anterior
+            # Obtener Empresas Nuevas
             df_nuevas_empresas = df_clientes_unique[
                 ~df_clientes_unique["Suministrador_final"].isin(
                     df_registro_cambios_empresas_mes_anterior["Suministrador_final"]
                 )
             ]
 
-            # Empresas Eliminadas
+            # Obtener Empresas Eliminadas
             df_empresas_eliminadas = df_registro_cambios_empresas_mes_anterior[
                 ~df_registro_cambios_empresas_mes_anterior["Suministrador_final"].isin(
                     df_clientes_unique["Suministrador_final"]
                 )
             ]
 
-            # Column of df to list
+            # Imprimir Empresas Nuevas y Eliminadas
             print(
                 "Empresas nuevas Mes Actual:",
                 df_nuevas_empresas["Suministrador_final"].to_list(),
@@ -265,7 +309,7 @@ class CreadorListaClientesBalance:
                 df_empresas_eliminadas["Suministrador_final"].to_list(),
             )
         
-            # If mes is not in registro_cambios by column Mes, add Columns Mes and Suministrador_final of df_clientes into registro_cambios
+            # Actualizar Registro Cambios Empresas
             if mes_fecha not in list(df_registro_cambios_empresas["Mes"].unique()):
             
                 print(
@@ -273,16 +317,15 @@ class CreadorListaClientesBalance:
                     + str(mes_fecha)
                 )
 
-                #df_clientes_unique["Mes"] = pd.to_datetime(df_clientes_unique["Mes"]).dt.strftime('%d-%m-%Y')
 
                 df_registro_cambios_empresas_final = pd.concat(
                     [
                         df_registro_cambios_empresas,
                         df_clientes_unique[["Suministrador_final", "Mes"]],
                     ]
-                )
+                ) # Concatenar df_registro_cambios_empresas y df_clientes_unique
 
-                # Rewrite df_registro_cambios_empresas_final into self.ruta_registro_cambios_empresas
+                # Guardar en archivo CSV
                 df_registro_cambios_empresas_final.to_csv(
                     self.ruta_registro_cambios_empresas, sep=";", index=False
                 )
@@ -290,16 +333,15 @@ class CreadorListaClientesBalance:
                 print(
                     "Revisar Base De Datos de Registro de Cambios de Empresas, el Mes Actual ya fue actualizado anteriormente"
                 )
-                #sys.exit() 
-                
-            # Path to save output Listado de Clientes
+                        
+            # Ruta de salida
             ruta_salida = r"\\nas-cen1\D.Peajes\\Cargo por Transmisión\02 Repartición\Balances\Listados de Clientes\Retiros Mensuales"
 
-            # Open an excel file to start saving dataframe each sheet
+            # Abrir archivo Excel
             with pd.ExcelWriter(
                 ruta_salida + "\\" + "Retiros_" + str(mes_numeral) + ".xlsx", engine="openpyxl"
             ) as writer:
-                # Write each dataframe to a different worksheet.
+                # Escibir en Excel los dataframes
                 df_clientes.to_excel(writer, sheet_name="Listado_Clientes", index=False)
 
                 df_clientes_R.to_excel(writer, sheet_name="Listado_Clientes_R", index=False)
